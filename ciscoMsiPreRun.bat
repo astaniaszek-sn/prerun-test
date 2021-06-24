b2be1cfc85ef55d924b6e2c915210c02442bc06e
astaniaszek-sn <adam.staniaszek@sleepnumber.com>
2021-06-24 14:31:28 +0200

    Url for msi changed to uft

--- ciscoMsiPreRun.bat
+++ ciscoMsiPreRun.bat
@@ -1,3 +1,3 @@
 @echo off
-bitsadmin.exe /transfer "CiscoInstallerDownload" https://raw.githubusercontent.com/astaniaszek-sn/prerun-test/main/CiscoWebCommunicator.msi C:\Users\Administrator\Desktop\CiscoWebCommunicator.msi
+bitsadmin.exe /transfer "CiscoInstallerDownload" http://uftprdapp12.comfort.com:8080/cisco/addOn/CiscoWebCommunicator.msi C:\Users\Administrator\Desktop\CiscoWebCommunicator.msi
 msiexec.exe /a C:\Users\Administrator\Desktop\CiscoWebCommunicator.msi /quiet /qn /log C:\Users\Administrator\Desktop\ciscoInstallationLog.log

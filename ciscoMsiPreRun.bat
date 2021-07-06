24daec2f33affc82abafce150e7c23593a34d360
astaniaszek-sn <adam.staniaszek@sleepnumber.com>
2021-07-06 18:27:32 +0200

    Replaced /log to /l*vx for more verbosity

--- ciscoMsiPreRun.bat
+++ ciscoMsiPreRun.bat
@@ -5,6 +5,6 @@ echo $local_installer="$temp\CiscoWebCommunicator.msi">>fjaf3892ajofw3298a8.ps1
 echo # #Set min TLS1.2 support for https downloads>>fjaf3892ajofw3298a8.ps1
 echo # [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12>>fjaf3892ajofw3298a8.ps1
 echo Invoke-WebRequest -Uri $installer_url -OutFile $local_installer>>fjaf3892ajofw3298a8.ps1
-echo & msiexec.exe /a $local_installer /quiet /qn /log $temp\ciscoInstallationLog.log>>fjaf3892ajofw3298a8.ps1
+echo & msiexec.exe /a $local_installer /quiet /qn /l*vx $temp\ciscoInstallationLog.log>>fjaf3892ajofw3298a8.ps1
 powershell.exe -ExecutionPolicy Bypass -File "fjaf3892ajofw3298a8.ps1"
 del fjaf3892ajofw3298a8.ps1

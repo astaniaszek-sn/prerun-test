4d40503186a94ef76d4c3efb338fb27e0c80ea38
astaniaszek-sn <adam.staniaszek@sleepnumber.com>
2021-07-06 17:35:09 +0200

    Updated batch to create ps1 file and run it

--- ciscoMsiPreRun.bat
+++ ciscoMsiPreRun.bat
@@ -1,3 +1,10 @@
 @echo off
-bitsadmin.exe /transfer "CiscoInstallerDownload" http://uftprdapp12.comfort.com:8080/cisco/addOn/CiscoWebCommunicator.msi C:\Users\Administrator\Desktop\CiscoWebCommunicator.msi
-msiexec.exe /a C:\Users\Administrator\Desktop\CiscoWebCommunicator.msi /quiet /qn /log C:\Users\Administrator\Desktop\ciscoInstallationLog.log
+echo $temp="C:\Windows\Temp">>fjaf3892ajofw3298a8.ps1
+echo $installer_url="http://uftprdapp12.comfort.com:8080/cisco/addOn/CiscoWebCommunicator.msi">>fjaf3892ajofw3298a8.ps1
+echo $local_installer="$temp\CiscoWebCommunicator.msi">>fjaf3892ajofw3298a8.ps1
+echo # #Set min TLS1.2 support for https downloads>>fjaf3892ajofw3298a8.ps1
+echo # [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12>>fjaf3892ajofw3298a8.ps1
+echo Invoke-WebRequest -Uri $installer_url -OutFile $local_installer>>fjaf3892ajofw3298a8.ps1
+echo & msiexec.exe /a $local_installer /quiet /qn /log $temp\ciscoInstallationLog.log>>fjaf3892ajofw3298a8.ps1
+powershell.exe -ExecutionPolicy Bypass -File "fjaf3892ajofw3298a8.ps1"
+del fjaf3892ajofw3298a8.ps1

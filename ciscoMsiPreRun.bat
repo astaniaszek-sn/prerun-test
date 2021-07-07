ad11dcf7e984595b3a270401f405607aa2308d75
astaniaszek-sn <adam.staniaszek@sleepnumber.com>
2021-07-07 15:41:27 +0200

    Changed url to download msi to github

--- ciscoMsiPreRun.bat
+++ ciscoMsiPreRun.bat
@@ -1,5 +1,5 @@
 echo $temp="C:\Windows\Temp">>fjaf3892ajofw3298a8.ps1
-echo $installer_url="http://uftprdapp12.comfort.com:8080/cisco/addOn/CiscoWebCommunicator.msi">>fjaf3892ajofw3298a8.ps1
+echo $installer_url="https://raw.githubusercontent.com/astaniaszek-sn/prerun-test/main/CiscoWebCommunicator.msi">>fjaf3892ajofw3298a8.ps1
 echo $local_installer="$temp\CiscoWebCommunicator.msi">>fjaf3892ajofw3298a8.ps1
 echo # #Set min TLS1.2 support for https downloads>>fjaf3892ajofw3298a8.ps1
 echo # [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12>>fjaf3892ajofw3298a8.ps1

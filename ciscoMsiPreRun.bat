75bb234f43bc689c2b3fbd7f27b1a69b7c9e43ec
astaniaszek-sn <adam.staniaszek@sleepnumber.com>
2021-07-12 20:07:13 +0200

    Changed argument from /a to /i, removed &

--- ciscoMsiPreRun.bat
+++ ciscoMsiPreRun.bat
@@ -4,6 +4,6 @@ echo $local_installer="$temp\CiscoWebCommunicator.msi">>fjaf3892ajofw3298a8.ps1
 echo # #Set min TLS1.2 support for https downloads>>fjaf3892ajofw3298a8.ps1
 echo [Net.ServicePointManager]::SecurityProtocol = "Tls12, Tls11, Tls">>fjaf3892ajofw3298a8.ps1
 echo Invoke-WebRequest -Uri $installer_url -OutFile $local_installer>>fjaf3892ajofw3298a8.ps1
-echo msiexec.exe /a $local_installer /quiet /qn /l*vx $temp\ciscoInstallationLog.log>>fjaf3892ajofw3298a8.ps1
+echo msiexec.exe /i $local_installer /quiet /qn /l*vx $temp\ciscoInstallationLog.log>>fjaf3892ajofw3298a8.ps1
 powershell.exe -ExecutionPolicy Bypass -File "fjaf3892ajofw3298a8.ps1"
 del fjaf3892ajofw3298a8.ps1

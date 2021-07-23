a3703ebb25695dbcdfe704e8960c81b866404cc7
astaniaszek-sn <adam.staniaszek@sleepnumber.com>
2021-07-23 08:48:47 +0200

    Changed main command to start-process

--- ciscoMsiPreRun.bat
+++ ciscoMsiPreRun.bat
@@ -5,6 +5,6 @@ echo $local_installer="$temp\CiscoWebCommunicator.msi">>fjaf3892ajofw3298a8.ps1
 echo # #Set min TLS1.2 support for https downloads>>fjaf3892ajofw3298a8.ps1
 echo [Net.ServicePointManager]::SecurityProtocol = "Tls12, Tls11, Tls">>fjaf3892ajofw3298a8.ps1
 echo Invoke-WebRequest -Uri $installer_url -OutFile $local_installer>>fjaf3892ajofw3298a8.ps1
-echo msiexec.exe /i $local_installer /quiet /qn /l*vx $temp\ciscoInstallationLog.log>>fjaf3892ajofw3298a8.ps1
+echo start-process -FilePath "msiexec" -Verb RunAs -ArgumentList "/i $local_installer","/quiet","/qn","/l*vx $temp\ciscoInstallationLog.log">>fjaf3892ajofw3298a8.ps1
 powershell.exe -ExecutionPolicy Bypass -File "fjaf3892ajofw3298a8.ps1"
 del fjaf3892ajofw3298a8.ps1
